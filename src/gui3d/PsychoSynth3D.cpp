63a45bc43305d63b81e9887622276f1743aad307
raskolnikov <Unknown>
2008-01-13 18:41:29 +0000

    Fullscreen

--- src/gui3d/PsychoSynth3D.cpp
+++ src/gui3d/PsychoSynth3D.cpp
@@ -104,7 +104,7 @@ void PsychoSynth3D::setupInput()
 #elif defined OIS_LINUX_PLATFORM 
     pl.insert(std::make_pair(std::string("x11_mouse_grab"), std::string("false"))); 
     pl.insert(std::make_pair(std::string("x11_mouse_hide"), std::string("true"))); 
-    pl.insert(std::make_pair(std::string("x11_keyboard_grab"), std::string("false"))); 
+    pl.insert(std::make_pair(std::string("x11_keyboard_grab"), std::string("true"))); 
     pl.insert(std::make_pair(std::string("XAutoRepeatOn"), std::string("true"))); 
 #endif
 
