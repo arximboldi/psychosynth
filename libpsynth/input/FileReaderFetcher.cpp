3779dc9e705651ba8038e6ad5c283564ad0dfa0c
raskolnikov <Unknown>
2008-03-30 04:44:32 +0000

    Forgot to add files.

---
+++ libpsynth/input/FileReaderFetcher.cpp
@@ -0,0 +1,186 @@
+/***************************************************************************
+ *                                                                         *
+ *   PSYCHOSYNTH                                                           *
+ *   ===========                                                           *
+ *                                                                         *
+ *   Copyright (C) Juan Pedro Bolivar Puente 2008                          *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <iostream>
+#include "input/FileReaderFetcher.h"
+
+using namespace std;
+
+namespace psynth
+{
+
+FileReaderFetcher::FileReaderFetcher(FileReader* reader,
+				     int buffer_size,
+				     int threshold) :
+    m_reader(reader),
+    m_buffer_size(buffer_size),
+    m_threshold(threshold),
+    m_tmp_buffer(AudioInfo(), m_threshold),
+    m_buffer(AudioInfo(), m_buffer_size),
+    m_read_ptr(m_buffer.begin()),
+    m_backwards(false),
+    m_read_pos(0),
+    m_new_read_pos(-1),
+    m_finished(false)
+{
+}
+
+void FileReaderFetcher::setBackwards(bool backwards)
+{
+    if (m_backwards != backwards) {
+	m_buffer_lock.lock();
+	m_reader_lock.lock();
+
+	m_backwards = backwards;
+	m_buffer.backwards();
+	m_read_ptr = m_buffer.sync(m_read_ptr);
+		
+	m_buffer_lock.unlock();
+	m_reader_lock.unlock();
+    }
+}
+
+void FileReaderFetcher::open(const char* file)
+{
+    m_buffer_lock.lock();
+    m_reader_lock.lock();
+    
+    m_reader->open(file);
+
+    setIsOpen(m_reader->isOpen());
+    setInfo(m_reader->getInfo());   
+    m_tmp_buffer.setInfo(getInfo(), m_threshold);
+    m_buffer.setAudioInfo(getInfo(), m_buffer_size);
+    
+    m_reader_lock.unlock();
+    m_cond.broadcast();
+    m_buffer_lock.unlock();
+}
+
+void FileReaderFetcher::seek(size_t pos)
+{
+    m_new_read_pos = pos;
+}
+
+int FileReaderFetcher::read(AudioBuffer& buf, int n_samples)
+{
+    int n_read;
+    
+    m_buffer_lock.lock();
+
+    if(m_buffer.availible(m_read_ptr) >= n_samples)
+	n_read = m_buffer.read(m_read_ptr, buf, n_samples);
+    else
+	n_read = 0;
+
+    if (m_buffer.availible(m_read_ptr) < m_threshold)
+	m_cond.broadcast();
+    m_buffer_lock.unlock();
+
+    if (m_backwards)
+	buf.reverse(n_read);
+    
+    return n_read;
+}
+
+void FileReaderFetcher::close()
+{
+    m_reader_lock.lock();
+
+    m_reader->close();
+    setIsOpen(m_reader->isOpen());
+
+    m_reader_lock.unlock();
+}
+
+void FileReaderFetcher::run()
+{
+    int n_read;
+    int must_read;
+    do {
+	/* Read the data. */
+	n_read = 0;
+	m_reader_lock.lock();
+	if (m_reader->isOpen()) {
+	    must_read = m_threshold;
+
+	    /* Do we have to seek? */
+	    if (m_new_read_pos >= 0) {
+		m_read_pos = m_new_read_pos;
+		m_new_read_pos = -1;
+		if (!m_backwards)
+		    m_reader->seek(m_read_pos);
+	    }
+
+	    /* Backwards reading needs seeking. */
+	    if (m_backwards) {
+		if (m_read_pos == 0)
+		    m_read_pos = getInfo().block_size - must_read;
+		else if (must_read > m_read_pos) {
+		    must_read = m_read_pos;
+		    m_read_pos = 0;
+		} else
+		    m_read_pos -= must_read;
+		m_reader->seek(m_read_pos);
+	    }
+
+	    n_read = m_reader->read(m_tmp_buffer, must_read);
+	    /*for (int i = 0; i < n_read; ++i)
+		cout << m_tmp_buffer[0][i] << endl;
+	    cout << "-----\n";
+	    */
+	    
+	    /* Check wether whe have finished reading and loop. */
+	    if (!n_read && !m_backwards) {
+		m_read_pos += n_read;
+		if (n_read == 0)
+		    m_new_read_pos = 0;
+	    }
+	}
+	m_reader_lock.unlock();
+
+	/* Add it to the buffer. */
+	if (n_read) {
+	    m_buffer_lock.lock();
+	    m_buffer.write(m_tmp_buffer, n_read);
+	    m_buffer_lock.unlock();
+	}
+
+	/* Wait until more data is needed. */
+	m_buffer_lock.lock();
+	while((!m_finished && m_buffer.availible(m_read_ptr) > m_threshold) ||
+	      !isOpen())
+	    m_cond.wait(m_buffer_lock);
+	m_buffer_lock.unlock();
+	
+    } while (!m_finished);
+}
+
+void FileReaderFetcher::finish()
+{
+    m_finished = true;
+    m_cond.broadcast();
+    join();
+    m_finished = false;
+}
+
+} /* namespace psynth */
---
+++ libpsynth/input/FileReaderFetcher.h
@@ -0,0 +1,89 @@
+/***************************************************************************
+ *                                                                         *
+ *   PSYCHOSYNTH                                                           *
+ *   ===========                                                           *
+ *                                                                         *
+ *   Copyright (C) Juan Pedro Bolivar Puente 2008                          *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef PSYNTH_FILEREADERFETCHER_H
+#define PSYNTH_FILEREADERFETCHER_H
+
+#include <libpsynth/input/FileReader.h>
+#include <libpsynth/common/Thread.h>
+#include <libpsynth/common/Mutex.h>
+#include <libpsynth/common/RingAudioBuffer.h>
+
+namespace psynth
+{
+
+class FileReaderFetcher : public FileReader,
+			  public SelfThread
+{
+    /* High default values for lower number of seeks when reading backwards. */
+    static const int DEFAULT_BUFFER_SIZE = 16384;
+    static const int DEFAULT_THRESHOLD  = 4096; 
+
+    FileReader* m_reader;
+    int m_buffer_size;
+    int m_threshold;
+    AudioBuffer m_tmp_buffer;
+    RingAudioBuffer m_buffer;
+    RingAudioBuffer::ReadPtr m_read_ptr;
+
+    bool m_backwards;
+    int m_read_pos;
+    int m_new_read_pos;
+
+    bool m_finished;
+    
+    Mutex m_reader_lock;
+    Mutex m_buffer_lock;
+    Condition m_cond;
+
+    void run();
+    
+public:
+    FileReaderFetcher(FileReader* reader = NULL,
+		      int buffer_size = DEFAULT_BUFFER_SIZE,
+		      int threshold  = DEFAULT_THRESHOLD);
+
+    void setFileReader(FileReader* reader) {
+	m_reader = reader;
+    }
+
+    FileReader* getFileReader() {
+	return m_reader;
+    }
+
+    bool getBackwards() const {
+	return m_backwards;
+    }
+    
+    void setBackwards(bool backwards);
+    
+    void open(const char* file);
+    void seek(size_t pos);
+    int read(AudioBuffer& buf, int n_samples);
+    void close();
+    void finish();
+};
+
+} /* namespace psynth */
+
+#endif /* PSYNTH_FILEREADERFETCHER_H */
+
